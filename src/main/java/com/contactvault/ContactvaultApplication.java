48730b82e0a63276265a8efc66591fb27ce2e30f
atharva11m <atharvamalode5@gmail.com>
2024-12-02 21:31:59 +0530

    #Project Creation

---
+++ src/main/java/com/contactvault/ContactvaultApplication.java
@@ -0,0 +1,13 @@
+package com.contactvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactvaultApplication.class, args);
+	}
+
+}
